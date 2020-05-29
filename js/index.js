487efb9528df5787cdd5ac686ecf526af6a51aa9
jguz17 <johnathanguzman17@gmail.com>
2020-05-29 00:20:35 -0500

    updates to comment section and simpson collection. Simpons render correctly when updated.

--- js/index.js
+++ js/index.js
@@ -4,8 +4,16 @@ const commentsAPI = 'http://localhost:3000/comments/'
 
 let user = {};
 let editSimpsonId;
+let editCommentSimpsonId;
+let editCommentUserId;
+let editCommentId;
+let deleteSimpsonId;
+let deleteCommentId;
+let testNode;
 const submitButton = document.querySelector('#trigger-simpson-form');
 const simpsonForm = document.querySelector('.form-container');
+const simpsonCollection = document.querySelector('#simpson-collection');
+
 // const commentForm = document.querySelector('#comment-form')
 
 const loginForm = document.querySelector('#login-form');
@@ -72,6 +80,7 @@ const fetchSimpsons = () => {
     fetch(simpsonsAPI)
     .then((results) => results.json())
     .then((simpsonData) => {
+        simpsonCollection.innerHTML = '';
         simpsonData.data.forEach(renderSimpsonToCollection);
         // console.log(simpsonData)
     })
@@ -81,7 +90,6 @@ const fetchSimpsons = () => {
 // render simpsons
 const renderSimpsonToCollection = (simpson) => {
     const { id, attributes } = simpson;
-    const simpsonCollection = document.querySelector('#simpson-collection');
     
     simpsonCollection.innerHTML += `<div class="simpson-card" data-id=${id}>
         <h3>${attributes.name}</h3>
@@ -97,21 +105,32 @@ const renderSimpsonToCollection = (simpson) => {
 console.log(document.querySelector('button'))
 
 const handleSimpsonCardClick = (event) => {
-    editSimpsonId = event.target.dataset.id;
+    // console.log(event.target.dataset.id)
+    // const testSimpId = event.target.dataset.id;
+    // ---- BEGIN SIMPSON CARD UPDATE --- 
     if(event.target.id === "edit") {
-        console.log("edit");
-        document.querySelector('body').innerHTML = `<div>
-            <form id="edit-simpson-card">
-                <input id="newName" name="name" placeholder="Enter new name"></input>
-                <input id="newQuote" name="quote" placeholder="Enter new quote"></input>
-                <input id="newLink" name="link" placeholder="Enter new link"></input>
+        console.log(event.target.parentElement);
+        // document.querySelector('.wrapper').style.display = 'none';
+        // document.querySelector('body').id = 'edit-simpson-body';
+        // document.querySelector('#edit-simpson-body');
+        event.target.parentElement.innerHTML += `<form id="edit-simpson-card">
+                                                    <input id="newName" name="name" placeholder="Enter new name"></input>
+                                                    <input id="newQuote" name="quote" placeholder="Enter new quote"></input>
+                                                    <input id="newLink" name="link" placeholder="Enter new link"></input>
 
-                <input type="submit"></input>
-            </form>
-        </div>`
+                                                    <input type="submit"></input>
+                                                </form>`
+        
+        // listen for submit on form and call handleEditSimpsonCard
         document.querySelector('#edit-simpson-card').addEventListener('submit', handleEditSimpsonCard);
     } else if(event.target.id === "delete") {
         console.log('delete')
+        document.querySelector('.wrapper').style.display = 'block';
+        deleteSimpsonId = event.target.parentElement.dataset.id;
+
+        fetch(simpsonsAPI + deleteSimpsonId, { method: "DELETE" })
+        .then((results) => results.json())
+        .then((data) => console.log(data))
     } else {
         const simpsonId = event.target.parentElement.dataset.id;
         console.log(simpsonId);
@@ -119,23 +138,29 @@ const handleSimpsonCardClick = (event) => {
         .then((results) => results.json())
         .then((simpsonData) => renderSimpsonThread(simpsonData))
     }
-    
 }
 
+// ---- CONTINUE SIMPSON CARD UPDATE ----
+// GET values from form and call function with values as objects/params
 const handleEditSimpsonCard = (event) => {
     event.preventDefault();
-    console.log(editSimpsonId);
+    const simpTargetId = event.target.parentElement.dataset.id;
     const newSimpsonName = event.target.name.value;
     const newSimpsonQuote = event.target.quote.value;
     const newSimpsonUrl = event.target.link.value;
+    
     // console.log(newSimpsonName, newSimpsonQuote, newSimpsonUrl);
     // console.log(newSimpsonName)
 
-    submitNewSimpson( {newSimpsonName, newSimpsonQuote, newSimpsonUrl} )
+    // call function with values as objects/params
+    submitNewSimpson( {newSimpsonName, newSimpsonQuote, newSimpsonUrl, simpTargetId} )
 }
 
-const submitNewSimpson = ({newSimpsonName, newSimpsonQuote, newSimpsonUrl}) => {
+// ---- CONTINUE SIMPSON CARD UPDATE ----
+//
+const submitNewSimpson = ({newSimpsonName, newSimpsonQuote, newSimpsonUrl, simpTargetId}) => {
     // console.log(newSimpsonName)
+    console.log(event.target.parentElement);
     console.log(newSimpsonName);
     console.log(newSimpsonQuote);
     console.log(newSimpsonUrl);
@@ -146,7 +171,7 @@ const submitNewSimpson = ({newSimpsonName, newSimpsonQuote, newSimpsonUrl}) => {
 
 
     console.log(editSimpsonId);
-    fetch(simpsonsAPI + editSimpsonId, {
+    fetch(simpsonsAPI + simpTargetId, {
         method: "PATCH",
         headers: {
             'Content-Type': 'application/json',
@@ -159,20 +184,32 @@ const submitNewSimpson = ({newSimpsonName, newSimpsonQuote, newSimpsonUrl}) => {
         })
     })
     .then((results) => results.json())
-    .then((data) => renderSimpsonToCollection(data))
+    .then((data) => {
+
+        fetchSimpsons()
+    })
     .catch((error) => console.log(error))
 }
 
 const renderEditedSimpson = (simpson) => {
-    const simpsonCollection = document.querySelector('#simpson-collection');
-    console.log(simpson)
-    // simpsonCollection.innerHTML += `<div class="simpson-card" data-id=${id}>
-    //     <h3>${attributes.name}</h3>
-    //     <p>${attributes.quote}</p>
-    //     <img src="${attributes.image}" class="simpson-image">
-    //     <button id="edit" class="button-class" data-id=${id}>edit</button>
-    //     <button id="delete" class="button-class" data-id=${id}>delete</button>
-    // </div>`;
+    // document.querySelector('.ghost-test').style.display = "none"
+    // document.querySelector('.wrapper').style.display = "block"
+    // const simpsonCollection = document.querySelector('#simpson-collection');
+    const identifiedSimpsonCard = document.querySelector(".simpson-card").dataset.id;
+    console.log(editSimpsonId)
+    if(simpson.id === editSimpsonId) {
+        console.log(simpson)
+        identifiedSimpsonCard.innerHTML = `<div class="simpson-card" data-id=${simpson.id}>
+            <h3>${simpson.name}</h3>
+            <p>${simpson.quote}</p>
+            <img src="${simpson.image}" class="simpson-image">
+            <button id="edit" class="button-class" data-id=${simpson.id}>edit</button>
+            <button id="delete" class="button-class" data-id=${simpson.id}>delete</button>
+        </div>`;
+    } else {
+        console.log("F")
+    }
+    
 }
 
 const renderSimpsonThread = (simpsonData) => {
@@ -187,12 +224,77 @@ const renderSimpsonThread = (simpsonData) => {
                             <form id="comment-form" class="comment-form">
                                 <input type="text" name="comment" placeholder="Enter a comment . . ."></input>
                                 <input id="post-simpson-comment" type="submit" name="submit" value="Comment" data-id=${simpsonId}>
-                            </form>`
-                            ;
+                            </form>`;
+    document.querySelector('#post-thread').addEventListener('click', handleButtonClicks);
     document.querySelector('#comment-form').addEventListener('submit', handleCommentFormSubmit);
 }
+
+const handleButtonClicks = (event) => {
+    editCommentSimpsonId = document.querySelector('#post-simpson-comment').dataset.id;
+    editCommentUserId = event.target.parentElement.dataset.userId;
+    editCommentId = event.target.parentElement.dataset.commentId;
+    // console.log(editCommentSimpsonId);
+    // console.log(editCommentUserId);
+    // console.log(editCommentId);
+    if(event.target.id === 'edit-comment-icon') {
+        const testThis = event.target.previousSibling;
+
+        document.querySelector('body').innerHTML = `<div>
+            <form id="edit-comment-form">
+                <input id="new-comment" name="content" placeholder="Enter new content"></input>
+
+                <input type="submit"></input>
+            </form>
+        </div>`
+
+        document.querySelector('#edit-comment-form').addEventListener('submit', handleEditCommentFormSubmit)
+    } else if(event.target.id === 'delete-comment-icon') {
+        console.log("thrash")
+        deleteCommentId = event.target.parentElement.dataset.commentId;
+
+        fetch(commentsAPI + deleteCommentId, { method: "DELETE" })
+        .then((results) => results.json())
+        .then((data) => console.log(data))
+    }
+}
+
+const handleEditCommentFormSubmit = (event) => {
+    event.preventDefault();
+    const newCommentContent = document.querySelector('#new-comment').value;
+    // console.log(newCommentContent)
+    // console.log(editCommentSimpsonId);
+    // console.log(editCommentUserId);
+    // console.log(editCommentId);
+
+    submitNewComment({ editCommentSimpsonId, editCommentUserId, editCommentId, newCommentContent })
+}
+
+const submitNewComment = ({ editCommentSimpsonId, editCommentUserId, editCommentId, newCommentContent }) => {
+    console.log(newCommentContent)
+    console.log(editCommentSimpsonId);
+    console.log(editCommentUserId);
+    console.log(editCommentId);
+
+    fetch(commentsAPI + editCommentId, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            content: newCommentContent,
+            simpson_id: editCommentSimpsonId,
+            user_id: editCommentUserId
+        })
+    })
+    .then((results) => results.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+}
+
 const renderComment = (comment) => {
-    return `<li>${comment.content}</li>`;
+    console.log(comment)
+    return `<li data-comment-id=${comment.id} data-user-id=${comment.user_id}><span>user #${comment.user_id}</span> : ${comment.content}<i id="edit-comment-icon" class="fas fa-edit"></i><i id="delete-comment-icon" class="fas fa-trash"></i></li>`;
 }
 
 const renderComments = (simpsonData) => {
